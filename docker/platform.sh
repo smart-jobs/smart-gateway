8a97ba04dd4ce0a0d637a0927781c811852d8915
dygapp <dygapp@qq.com>
2019-03-15 14:06:56 +0800

    add restart options

--- docker/platform.sh
+++ docker/platform.sh
@@ -1,1 +1,1 @@
-docker run --name gateway-platform --network host -v /home/smart/gateway:/app -w /app -d java:latest java -jar smart-gateway-1.1.0.jar --spring.profiles.active=platform-prod
+docker run --name gateway-platform --network host --restart always -v /home/smart/gateway:/app -w /app -d java:latest java -jar smart-gateway-1.1.0.jar --spring.profiles.active=platform-prod
--- docker/weixin.sh
+++ docker/weixin.sh
@@ -1,1 +1,1 @@
-docker run --name gateway-weixin --network host -v /home/smart/gateway:/app -w /app -d java:latest java -jar smart-gateway-1.1.0.jar --spring.profiles.active=weixin-prod
+docker run --name gateway-weixin --network host --restart always -v /home/smart/gateway:/app -w /app -d java:latest java -jar smart-gateway-1.1.0.jar --spring.profiles.active=weixin-prod
